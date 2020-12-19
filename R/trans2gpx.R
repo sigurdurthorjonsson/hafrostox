972f02861b30cc4299962f8cf9ac50c70039a485
Sigurður Þór Jónsson <sigurdur@hafro.is>
2020-12-19 10:21:28 +0000

    add trans2gpx and update vignette

---
+++ R/trans2gpx.R
@@ -0,0 +1,38 @@
+#' Splits transects by strata and writes gpx-files to a given directory.
+#'
+#' @param file Name of gpx output file
+#' @param lat,lon Latitude and longitude of route-points.
+#' @param name Name of route-point, e.g 1 thru number of rte-points.
+#' @keywords gpx, lat, lon, route
+#' @return Writes a gpx-file of a single route.
+#' @export
+#' @examples
+#'   strata <- gpx2sf(
+#'     paste(system.file("extdata",package="hafrostox"),"gpx",sep="/"))
+#'   fn <- tempfile()
+#'   sf2wkt(strata,fn)
+#'   zz <- surveyPlanner(fn,type="RectEnclZZ", bearing=c("NE","W","N","E"),
+#'     nmi=1000,knots=10,seed=1,equalEffort=TRUE,retour=TRUE)
+#'   dn <- tempdir()
+#'   dn
+#'   trans2gpx(zz, dn)
+
+trans2gpx <- function(surv,dir_name) {
+  trans <- surv$Transect[,c("stratum","lat_start","lon_start","lat_stop","lon_stop")]
+  trans <- split.data.frame(trans, trans$stratum)
+  start <- lapply(trans, function(x) {
+    x %>% dplyr::select(lat=lat_start,lon=lon_start)
+    })
+  stop <- lapply(trans, function(x) {
+    x %>% dplyr::select(lat=lat_stop,lon=lon_stop) %>%
+      dplyr::filter(dplyr::row_number()==nrow(.))
+    })
+  trans <- purrr::pmap(list(start,stop),dplyr::bind_rows)
+
+  for (i in seq(along=trans)) {
+    df <- trans[[i]]
+    df$name <- 1:nrow(df)
+    gpx_rte(file = paste(dir_name,paste0("trans_strata",i,".gpx"),sep="/"),
+      lat=df$lat,lon=df$lon,name=df$name)
+  }
+}
